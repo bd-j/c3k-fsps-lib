68ead0d0eec1e6856e2824ac35f366fdd971f685
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2025-07-30 14:18:38 -0400

    add sapphire partition

--- jobs/ody_copy.sh
+++ jobs/ody_copy.sh
@@ -4,7 +4,7 @@
 #SBATCH -n 1 # Number of cores requested
 #SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -t 1:00:00 # Runtime
-#SBATCH -p conroy_priority,shared # Partition to submit to
+#SBATCH -p conroy_priority,shared,sapphire # Partition to submit to
 #SBATCH --mem-per-cpu=4000 # Memory per node in MB (see also --mem-per-cpu)
 #SBATCH -o logs/c3k_copy_%A.log # Standard out goes to this file
 #SBATCH -e logs/c3k_copy_%A.log # Standard err goes to this file
--- jobs/ody_resample.sh
+++ jobs/ody_resample.sh
@@ -4,7 +4,7 @@
 #SBATCH -n 1 # Number of cores requested
 #SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -t 6:00:00 # Runtime
-#SBATCH -p conroy_priority,shared # Partition to submit to
+#SBATCH -p conroy_priority,shared,sapphire # Partition to submit to
 #SBATCH --mem-per-cpu=2500 # Memory per node in MB (see also --mem-per-cpu)
 #SBATCH -o logs/c3k_resample_%A_%a.log # Standard out goes to this file
 #SBATCH -e logs/c3k_resample_%A_%a.log # Standard err goes to this file
